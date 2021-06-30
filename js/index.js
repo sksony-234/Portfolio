94d355f8347d0d502b80498332d403019bf2b1a0
sksony <sksony2698@gmail.com>
2021-06-30 18:15:05 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// javascript for navigation bar 
+window.addEventListener('scroll', function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY>0);
+});
+
+// navigation bar 
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+const navigationItems = document.querySelectorAll('.navigation a');
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) =>{
+    navigationItem.addEventListener("click", () =>{
+        menuBtn.classList.remove("active");
+        navigation.classList.remove("active");
+
+    });
+});
